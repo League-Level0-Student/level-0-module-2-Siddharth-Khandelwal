9f7c073173f29df013eed2493bb22d35f2cb4ba6
root <root@d5ad4249d6a6>
2020-08-26 00:06:15 +0000

    voting booth

--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -5,21 +5,32 @@ import javax.swing.JOptionPane;
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
-		
+		public static void main(String[] args) {
+			
+	
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
-		
+	String heightToRide = JOptionPane.showInputDialog("How tall are you(Use Inches)");
 		// Uncomment the line below...
 			//if( heightToRide > 42 ) { }
 		
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
 		// 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... 
-	
+			int heightAsInt = Integer.parseInt(heightToRide);
 		// 4) If the heightAsInt is greater than or equal to 48 inches tall print:
 	    // "Hurray! You are tall enough to ride the coaster alone!" Hint: use JoptionPane
-		
+		if(heightAsInt>=48) {
+			JOptionPane.showMessageDialog(null, "You can go on this ride!");
+		}
+	
 		// 5) Else if the heightAsInt is greater than or equal to 42 inches tall print:
 		// "You can ride the coaster with someone else!" 
-	
+		else if(heightAsInt>=42) {
+			JOptionPane.showMessageDialog(null, "You can ride with an adult!");
+		}
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
-} 
+		else {  System.out.println("You must be at least 42 inches tall");
+		} 
+
+}
+}
---
+++ src/_03_string_conversion/_4_voting_booth/Voting_Booth.java
@@ -0,0 +1,16 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class Voting_Booth {
+public static void main(String[] args) {
+	String age = JOptionPane.showInputDialog("How old are you?");
+	int Age = Integer.parseInt(age);
+	if(Age>=18) {
+		JOptionPane.showInputDialog("Who should be the next president?");
+	}
+	if(Age<18) {
+		
+	}
+}
+}
